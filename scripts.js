ade5b6ea3a4c184e3f756a4e0b7e8db561abd71e
Blues 🦋 <yanis.anciaux.dwwm48@gmail.com>
2025-01-30 09:23:44 +0100

    uptade yanis

--- scripts.js
+++ scripts.js
@@ -1,79 +1,64 @@
+const skillColors = {
+    "bootstrap": "blue",
+    "html5": "red",
+    "symfony": "yellow",
+    "css3": "blue",
+    "javascript": "green",
+    "vue.js": "green",
+    "figma": "pink",
+    "php": "yellow",
+    "c#": "purple",
+    "tailwind": "blue"
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const apprenantsContainer = document.getElementById('apprenants-container');
 
-    // Fetch les données des promotions et les stocker dans une map pour une recherche rapide
     fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/promotions')
         .then(response => response.json())
         .then(promotions => {
-            // Créer une map des promotions pour une recherche rapide
             const promotionMap = {};
             promotions.forEach(promotion => {
                 promotionMap[promotion.id] = promotion.slug;
             });
 
-            // Fetch les compétences et les stocker dans une map pour une recherche rapide
             fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/competences')
                 .then(response => response.json())
                 .then(competences => {
-                    // Créer une map des compétences pour une recherche rapide
                     const competencesMap = {};
                     competences.forEach(competence => {
                         competencesMap[competence.id] = competence.name;
                     });
 
-                    // Fetch la liste des apprenants avec une limite de 100 par page
                     fetch('http://portfolios.ruki5964.odns.fr/wp-json/wp/v2/apprenants?per_page=100')
                         .then(response => response.json())
                         .then(apprenants => {
                             apprenants.forEach(apprenant => {
-                                // Trouver le nom de la promotion
-                                const promotionId = apprenant.promotions[0]; // Supposer que chaque apprenant a une promotion
+                                const promotionId = apprenant.promotions[0];
                                 const promotionName = promotionMap[promotionId] || 'Unknown';
 
-                                // Trouver les noms des compétences et attribuer des classes de couleurs
                                 const competencesElements = apprenant.competences.map(skillId => {
                                     const skillName = competencesMap[skillId] || 'Unknown';
-                                    let colorClass;
-                                    switch(skillName) {
-                                        case 'HTML5':
-                                            colorClass = 'font-semibold bg-red-400';
-                                            break;
-                                        case 'CSS':
-                                            colorClass = 'font-semibold bg-sky-500';
-                                            break;
-                                        case 'TailwindCSS':
-                                            colorClass = 'font-semibold bg-teal-500';
-                                            break;
-                                        case 'JavaScript':
-                                            colorClass = 'font-semibold bg-yellow-500';
-                                            break;
-                                        case 'Figma':
-                                            colorClass = 'font-semibold bg-purple-600';
-                                            break;
-                                        default:
-                                            colorClass = 'font-semibold bg-gray-800';
-                                    }
+                                    const colorClass = skillColors[skillName.toLowerCase()] || 'gray';
                                     return `<span class="skill ${colorClass}">${skillName}</span>`;
                                 });
 
-                                // Créer un élément de carte
                                 const card = document.createElement('div');
                                 card.className = 'card';
                                 card.setAttribute('onclick', 'flipCard(this)');
 
-                                // Remplir la carte avec les données de l'apprenant
                                 card.innerHTML = `
                                     <div class="card-inner">
                                         <div class="card-front">
-                                            <h2 class="name">${apprenant.nom}<br>${apprenant.prenom}</h2>
-                                            <img class="profilePic" src="${apprenant.image}"/>
+                                            <h2 class="name">${apprenant.title.rendered}</h2>
+                                            <div class="container-image"><img class="profilePic" src="${apprenant.image}"></div>
                                             <p class="promo">Promotion: ${promotionName}</p>
-                                            <p class="skills">${competencesElements.join('')}</p>
+                                            <div class = "container-skill"><p class="skills">${competencesElements.join('')}</p></div>
                                             <div class="links">
-                                                <a href="${apprenant.urlgit}" target="_blank"><img src="logoGithub.svg"></a>
-                                                <a href="${apprenant.linkedin}" target="_blank"><img src="logoLinkedin.svg"></a>
-                                                <a href="${apprenant.cv}" target="_blank"><img src="logoCV.svg"></a>
-                                                <a href="${apprenant.portfolio}" target="_blank"><img src="logoPortfolio.svg"></a>
+                                                <a href="${apprenant.urlgit}" target="_blank"><img src="assets/icones/logoGithub.svg"></a>
+                                                <a href="${apprenant.linkedin}" target="_blank"><img src="assets/icones/logoLinkedin.svg"></a>
+                                                <a href="${apprenant.cv}" target="_blank"><img src="assets/icones/logoCV.svg"></a>
+                                                <a href="${apprenant.portfolio}" target="_blank"><img src="assets/icones/logoPortfolio.svg"></a>
                                             </div>
                                         </div>
                                         <div class="card-back">
@@ -82,18 +67,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
                                     </div>
                                 `;
 
-                                // Ajouter la carte au conteneur
                                 apprenantsContainer.appendChild(card);
                             });
                         })
-                        .catch(error => console.error('Erreur:', error)); // Gérer les erreurs pour le fetch des apprenants
+                        .catch(error => console.error('Erreur:', error));
                 })
-                .catch(error => console.error('Erreur:', error)); // Gérer les erreurs pour le fetch des compétences
+                .catch(error => console.error('Erreur:', error));
         })
-        .catch(error => console.error('Erreur:', error)); // Gérer les erreurs pour le fetch des promotions
+        .catch(error => console.error('Erreur:', error));
 });
 
-// Fonction pour retourner la carte
+
+
 function flipCard(card) {
     card.classList.toggle('flipped');
 }
